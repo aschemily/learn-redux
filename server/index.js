cb2bf7d5ac96f075b720d3886b0a77acbd4d1dc1
aschemily <aschemily@gmail.com>
2019-05-20 20:35:25 -0400

    node js

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
 app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+  // const name = req.query.name || 'World';
+  // res.setHeader('Content-Type', 'application/json');
+  // res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
 app.listen(3001, () =>
